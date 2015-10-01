a66a3bb86d230fc7158e71ba320160b421c7cc6d
Shan Shi <a455898334@gmail.com>
2015-09-30 23:06:42 -0400

    works

--- project2/mySystem.c
+++ project2/mySystem.c
@@ -14,10 +14,6 @@
 
 /* Take an unknown shell command from myshell and try to run that command in the system */
 void mySystem(char * command[], int num_args, char *searchpaths[], int numpaths){
-	const char delim[1] = " "; // delimeter is a space, null, or newline
-	char * token;
-	int curr_pos = 0;
-
 	int pID = fork();
 	if(pID == 0){
 		for(int i = 0; i < numpaths; i++){
